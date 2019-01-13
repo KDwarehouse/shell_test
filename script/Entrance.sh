d14ca15aeba797076b67ac3716e34972388bec21
df <df@qq.com>
2019-01-13 23:59:48 +0800

    modify file

--- script/Entrance.sh
+++ script/Entrance.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 mkdir /tmp/work
 cd /tmp/work
-git clone https://github.com/KDwarehouse/shell_test.git
-bash /tmp/work/shell_test/script/menu.sh
+git clone https://github.com/KDwarehouse/shell_test.git && bash /tmp/work/shell_test/script/menu.sh
