1d65b7cdfe947fe97a0ed8075a2ab3c6dfa14a05
df <df@qq.com>
2019-01-14 21:04:27 +0800

    add guolv fangwencishu

---
+++ test/test1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+awk  '{ip[$1]++} END{for(i in ip) {print ip[i],i}}' /root/gaun.com.log | sort -nr > /tmp/gaun.com.log
+num_file=`awk '{print $1}' /tmp/gaun.com.log`
+ip_file=`awk '{print $2}' /tmp/gaun.com.log`
+a=`sed -n '$=' /tmp/gaun.com.log`
+
+numa=`wc -l /tmp/gaun.com.log | awk '{print $1}'`
+numb=$[numa/2]
+#numc=`awk -v i=${numb} '{print $i} /tmp/gaun.com.log'`
+numc=`sed -n "${numb}p" /tmp/gaun.com.log | awk '{print $1}'`
+for i in ${num_file}
+do
+	if [ $i -gt $[numc+1500] ];then
+		echo $i 
+		b=`grep $i /tmp/gaun.com.log | awk '{print $2}'`
+		echo $b
+	else
+		continue
+	fi
+	
+done
