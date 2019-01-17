fe9856361da1202e9afe842dbce073a4df02464a
df <df@163.com>
2019-01-18 01:14:01 +0800

    over

--- script/install_bt.sh
+++ script/install_bt.sh
@@ -1,10 +1,15 @@
 #/bin/bash
-echo "安装宝塔面板"
+echo -e "\033[41;30m 准备安装宝塔面板...... \033[0m"
+
 yum -y update
 yum install -y wget && wget -O install.sh http://download.bt.cn/install/install.sh && sh install.sh
 echo ''
 echo ''
-echo ''
-echo ''
+if [ $? -eq 0 ];then
+	echo -e "\033[42;38m 安装宝塔面板成功 \033[0m"
+else
+	echo -e "\033[41;30m error 安装失败"
+	exit
+fi
 echo ''
 echo ''
