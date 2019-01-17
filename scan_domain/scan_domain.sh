82c37ceb57d58866621153ccbddab8fc5c645050
df <df@163.com>
2019-01-18 05:23:05 +0800

    添加自动检查域名脚本

---
+++ scan_domain/scan_domain.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source /root/send_message.sh
+URL_LIST=`awk '{print $1}' /root/ce_url_list.txt`
+DATE=`date +%F-%H:%M`
+#echo ${DATE}
+for URL in ${URL_LIST}
+do
+	status_code=`curl -o /dev/null -s -w %{http_code} "${URL}"`
+	INFO_NAME=`grep ${URL} /root/ce_url_list.txt | awk '{print $2}'`
+	if [ ${status_code} -lt 200 -o ${status_code} -ge 400 ];then
+		text="${DATE},${INFO_NAME}${URL}有问题，请检查!"
+		send_message ${text}
+	else
+		continue
+	fi
+done
---
+++ scan_domain/send_message.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+function send_message(){
+        # main机器人token
+        #tele_token='748091669:AAHH0lF3qVj4hD5qHFp4qQowyqbAwdh67xU'
+        #group_id='-390922010'
+        #测试chat_id
+        tele_token='619423079:AAFPQuGFbCwH8O3jSefntGa8rd0Tr_Wq_zs'
+        group_id='-342008533'
+        text="<b>angry发现</b> »> "$1
+        curl "https://api.telegram.org/bot${tele_token}/sendMessage?chat_id=${group_id}&text=${text}&parse_mode=html"
+}
