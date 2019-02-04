92f600df08a9c74cd09103cf036a80e822c97be5
df <df@163.com>
2019-02-04 20:46:35 +0800

    over

--- script/set_selinuxfirewalld.sh
+++ script/set_selinuxfirewalld.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-echo "设置selinux为disabled....."
+echo -e "\033[41;37m 设置selinux为disabled.....\033[0m"
 setenforce 0
 sed -i 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/selinux/config
 sed -n  '/^SELINUX=/p' /etc/selinux/config | grep disabled
 if [ $? -eq 0 ];then
-	echo "设置成功！！！"
+	echo -e "\033[42;37m selinux设置成功！！！\033[0m"
 else
-	echo "设置失败！！！，请退出查看......" && exit
+	echo -e "\033[41;37m 设置失败！！！，请退出查看......\033[0m" && exit
 fi
 yum remove firewalld 
 rpm -q firewalld
 if [ $? -gt 0 ];then
-	echo "firewalld卸载成功！！！"
+	echo -e "\033[42;37m firewalld卸载成功！！！\033[0m"
 else
-	echo "firewalld卸载失败！！！，请退出查看......" && exit
+	echo -e "\033[41;37m firewalld卸载失败！！！，请退出查看......\033[0m" && exit
 fi
-echo ''
