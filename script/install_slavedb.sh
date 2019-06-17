1d5093c660e71bf4b4f55925fd0055cf539d062e
df <df@example.com>
2019-06-17 14:45:50 +0800

    modify db

--- script/install_slavedb.sh
+++ script/install_slavedb.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-if [ -d /tmp/soft ];then
-        mkdir /tmp/softsoft11
-        cd /tmp/softsoft11
-        yum -y install autoconf perl-JSON perl wget expect
-        yum -y remove mariadb*
-        wget https://cdn.mysql.com//Downloads/MySQL-5.7/mysql-5.7.24-1.el7.x86_64.rpm-bundle.tar
-        tar xf mysql-5.7.24-1.el7.x86_64.rpm-bundle.tar
-else
-        mkdir /tmp/soft
-        cd /tmp/soft
-        yum -y install autoconf perl-JSON perl wget expect
-        yum -y remove mariadb*
-        wget https://cdn.mysql.com//Downloads/MySQL-5.7/mysql-5.7.24-1.el7.x86_64.rpm-bundle.tar
-        tar xf mysql-5.7.24-1.el7.x86_64.rpm-bundle.tar
-fi
-
 if [ -d /var/lib/mysql ];then
         echo -e "\033[41;37m MYSQL已经安装 \033[0m"
         read -p "1）卸载并重新安装 2）退出" user_chi
@@ -34,17 +18,36 @@ if [ -d /var/lib/mysql ];then
                 exit;;
         esac
 fi
-mv mysql-community-server-minimal-5.7.24-1.el7.x86_64.rpm /tmp
-yum -y install mysql-*.rpm
-\cp -rf /tmp/work/shell_test/conf/slave.cnf /etc/my.cnf
-systemctl start mysqld
+if [ -d /tmp/soft ];then
+        mkdir /tmp/softsoft11
+        cd /tmp/softsoft11
+        yum -y install autoconf perl-JSON perl wget expect
+        yum -y remove mariadb*
+        wget https://cdn.mysql.com//Downloads/MySQL-5.7/mysql-5.7.24-1.el7.x86_64.rpm-bundle.tar
+        tar xf mysql-5.7.24-1.el7.x86_64.rpm-bundle.tar
+	mv mysql-community-server-minimal-5.7.24-1.el7.x86_64.rpm /tmp
+	rm -rf mysql-5.7.24-1.el7.x86_64.rpm-bundle.tar
+	yum -y install mysql-community-*.rpm
+	systemctl start mysqld
+else
+        mkdir /tmp/soft
+        cd /tmp/soft
+        yum -y install autoconf perl-JSON perl wget expect
+        yum -y remove mariadb*
+        wget https://cdn.mysql.com//Downloads/MySQL-5.7/mysql-5.7.24-1.el7.x86_64.rpm-bundle.tar
+        tar xf mysql-5.7.24-1.el7.x86_64.rpm-bundle.tar
+	mv mysql-community-server-minimal-5.7.24-1.el7.x86_64.rpm /tmp
+	rm -rf mysql-5.7.24-1.el7.x86_64.rpm-bundle.tar
+	yum -y install mysql-community-*.rpm
+	systemctl start mysqld
+fi
 ss -nutlp | grep mysqld
 if [ $? -eq 0 ];then
         echo "mysql安装已经完成！"
 else
         echo "mysql安装失败！"
 fi
-
+\cp -rf /tmp/work/shell_test/conf/slave.cnf /etc/my.cnf
 read -p "请输入你要创建的密码：" passwdb
 passwda=`awk '/password/{print $NF}' /var/log/mysqld.log`
 expect <<  EOF
@@ -54,17 +57,13 @@ expect "mysql>" {send "SET PASSWORD FOR root@localhost=PASSWORD('${passwdb}');\r
 expect "mysql>" {send "exit\r"}
 EOF
 [ $? -eq 0 ] && echo mysql密码修改成功,密码是:${passwdb} || echo 密码修改失败！
-
 read -p "设置从库ID编号：" id_a
 read -p "设置库binlog日志代号：" a_name
 read -p "设置mysql端口号：" port_a
 sed -i "s/server_id=1/server_id=${id_a}/" /etc/my.cnf
 sed -i "s/log_bin=master/log_bin=${a_name}/" /etc/my.cnf
 sed -i "s/port=4273/port=${port_a}/" /etc/my.cnf
-
 systemctl restart mysqld
-
-
 read -p "主库bin文件名为:" m_bin
 read -p "主库position位置为:" m_position
 read -p "主库内网ip：" m_ip
